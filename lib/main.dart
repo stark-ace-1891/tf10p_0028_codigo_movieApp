0dea28b9c3fa75fab9e2a1f37c2213015ca6593e
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-06-06 15:30:25 -0500

    Avance parte superior

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tf10p_0028_codigo_movieapp/pages/home_page.dart';
 
 void main(){
@@ -13,6 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "MovieApp",
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+      ),
       home: HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,52 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Prueba"),
+      backgroundColor: Color(0xff23232d),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Welcome, Fiorella",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "Discover",
+                            style: TextStyle(
+                              height: 1.25,
+                              color: Colors.white,
+                              fontSize: 38,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Color(0xff3DDFB3),
+                      radius: 30,
+                      child: CircleAvatar(
+                        radius: 26,
+                        backgroundImage: NetworkImage("https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
